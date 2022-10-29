c1c3fee51cb037311e0365802186622e845b19a7
tvaccari34 <tiago.vaccari@outlook.com>
2022-10-29 18:58:04 +0100

    Added feature to change the profile image

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -7,7 +7,9 @@
             <div class="col-lg-6">
                 <div class="card"><br><br>
                     <center>
-                        <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                        <img class="rounded-circle avatar-xl" src="{{
+                            (!empty($userData->profile_image)) ? url('upload/admin_images/' . $userData->profile_image) : url('upload/no_image.jpg') }}"
+                             alt="Card image cap">
                     </center>
 
                     <div class="card-body">
