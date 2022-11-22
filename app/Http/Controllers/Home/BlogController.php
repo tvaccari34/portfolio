fd45c06cd6a045576f0053a1b0d26f5deb491fd8
tvaccari34 <tiago.vaccari@outlook.com>
2022-11-22 19:19:46 +0000

    Add blog feature created

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Models\BlogCategory;
 use Illuminate\Support\Carbon;
 use Image;
 
@@ -16,6 +17,49 @@ class BlogController extends Controller
     }
 
     public function AddBlog(){
-        return view('admin.blog.blog_add');
+
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+
+        return view('admin.blog.blog_add', compact('categories'));
+    }
+
+    public function StoreBlog(Request $request){
+        $request->validate([
+            'blog_category_id' => 'required',
+            'blog_title' => 'required',
+            'blog_tags' => 'required',
+            'blog_description' => 'required',
+            'blog_image' => 'required',
+        ],[
+            'blog_category_id.required' => 'Blog category is required',
+            'blog_title.required' => 'Blog title is required',
+            'blog_tags.required' => 'Blog tags is required',
+            'blog_description.required' => 'Blog description is required',
+            'blog_image.required' => 'Blog image is required',
+        ]);
+
+        $image = $request->file('blog_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+
+        Image::make($image)->resize(430,327)->save('upload/blog/'.$name_gen);
+
+        $save_url = 'upload/blog/'.$name_gen;
+
+        Blog::insert([
+            'blog_category_id' => $request->blog_category_id,
+            'blog_title' => $request->blog_title,
+            'blog_tags' => $request->blog_tags,
+            'blog_description' => $request->blog_description,
+            'blog_image' => $save_url,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Blog Page Added successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.blog')->with($notification);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -94,7 +94,7 @@ Route::controller(BlogController::class)->group(function() {
     Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
     Route::get('/delete/blog/{id}', 'DeleteBlog')->name('delete.blog');
 
-    Route::post('/store/blog', 'StoreBlog')->name('story.blog');
+    Route::post('/store/blog', 'StoreBlog')->name('store.blog');
     Route::post('/update/blog', 'UpdateBlog')->name('update.blog');
 
 });
