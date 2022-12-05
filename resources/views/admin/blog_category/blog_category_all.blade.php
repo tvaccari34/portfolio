46284ef9c916f7b5c85b5b26910b7eee16963c1c
tvaccari34 <tiago.vaccari@outlook.com>
2022-12-05 10:05:30 +0000

    Use Key instead of auto increment variable for tables

--- resources/views/admin/blog_category/blog_category_all.blade.php
+++ resources/views/admin/blog_category/blog_category_all.blade.php
@@ -30,10 +30,9 @@
                             </thead>
 
                             <tbody>
-                                @php($i = 1)
-                                @foreach ($blogCategories as $item)
+                                @foreach ($blogCategories as $key => $item)
                                     <tr>
-                                        <td>{{$i++}}</td>
+                                        <td>{{$key+1}}</td>
                                         <td>{{$item->blog_category}}</td>
                                         <td>
                                             <a href="{{ route('edit.blog.category', $item->id) }}" class="btn btn-info sm" title="Edit Data"><i class="fas fa-edit"></i></a>
